c8ac8ae1697a7f3b516f1ba5f8a71ba6ea979012
Lukxi <Lukxi@users.noreply.github.com>
2022-09-30 11:20:40 +0200

    fixed preview head, changed title

--- src/main/java/me/lukas/JenoSign/GUIs/SignatureSettingsGUI.java
+++ src/main/java/me/lukas/JenoSign/GUIs/SignatureSettingsGUI.java
@@ -9,7 +9,9 @@ import me.oxolotel.utils.bukkit.menuManager.menus.CustomMenu;
 import me.oxolotel.utils.bukkit.menuManager.menus.SlotCondition;
 import me.oxolotel.utils.bukkit.menuManager.menus.content.InventoryContent;
 import me.oxolotel.utils.bukkit.menuManager.menus.content.InventoryItem;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
@@ -25,6 +27,7 @@ public class SignatureSettingsGUI extends CustomMenu implements Closeable, SlotC
         settings=new String[2];
         settings[0] = "normal";
         settings[1] = "name";
+        setTitle("§9§lSignatur Einstellungen");
     }
 
     @Override
@@ -36,7 +39,7 @@ public class SignatureSettingsGUI extends CustomMenu implements Closeable, SlotC
     public InventoryContent getContents(Player player) {
         c.fill(0, 53, new InventoryItem(new ItemStack(Material.GRAY_STAINED_GLASS_PANE), ()->{}));
         ItemStack i = new ItemStack(SignManager.signMap.get(player.getUniqueId()).createDeploySignSettings(settings));
-        ItemStack s = new SkullBuilder("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDRmN2JjMWZhODIxN2IxOGIzMjNhZjg0MTM3MmEzZjdjNjAyYTQzNWM4MjhmYWE0MDNkMTc2YzZiMzdiNjA1YiJ9fX0===",
+        ItemStack s = new SkullBuilder("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDRmN2JjMWZhODIxN2IxOGIzMjNhZjg0MTM3MmEzZjdjNjAyYTQzNWM4MjhmYWE0MDNkMTc2YzZiMzdiNjA1YiJ9fX0=",
                 "§9§lSign Preview").build();
         c.addGuiItem(16, new InventoryItem(s, ()->{}));
         c.addGuiItem(25, new InventoryItem( i, ()->{}));
@@ -62,11 +65,12 @@ public class SignatureSettingsGUI extends CustomMenu implements Closeable, SlotC
             settings[0] = "normal";
             InventoryMenuManager.getInstance().getOpenMenu(player).generateInventory();
         }));
-        ItemStack sJeno = new SkullBuilder("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzdmODMxNDRjNGNlMzJlOWVhM2E0NDhjY2M4NWZkOWM5YmU0M2MyYmY1Njg4ZWMyZGQ1MzNhMTk5NTY2MTA2NiJ9fX0==",
+        ItemStack sJeno = new SkullBuilder("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjgxZDJjMWE1YTU5YjU3NTM1OTQ4MmE4YTMzODU0Y2QzYjE2MzRkZDU0YzQ1YzRjYTFjYTEwZTg2MDdkNDcifX19",
                 "§5§lSigniere als Jeno-Team").build();
         ItemStack sPlayer = new ItemStack(Material.PLAYER_HEAD, 1, (short) 3);
+        OfflinePlayer op = Bukkit.getOfflinePlayer(player.getName());
         SkullMeta skullMeta = ((SkullMeta) sPlayer.getItemMeta());
-        skullMeta.setOwningPlayer(player);
+        skullMeta.setOwner(player.getName());
         sPlayer.setItemMeta(skullMeta);
         c.addGuiItem(31, new InventoryItem(sPlayer, ()->{
             settings[1] = player.getName();
