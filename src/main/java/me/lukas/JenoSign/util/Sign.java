1ba13480a5c3950388b815c64d32706d0d7dff48
Lukxi <Lukxi@users.noreply.github.com>
2022-10-01 13:20:20 +0200

    implemented line edit

--- src/main/java/me/lukas/JenoSign/util/Sign.java
+++ src/main/java/me/lukas/JenoSign/util/Sign.java
@@ -1,43 +1,36 @@
 package me.lukas.JenoSign.util;
 
-import com.google.common.collect.Lists;
 import me.lukas.JenoSign.JenoSign;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.user.User;
-import net.luckperms.api.node.Node;
-import net.luckperms.api.node.NodeType;
-import net.luckperms.api.platform.Platform;
+import org.bukkit.ChatColor;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class Sign {
 
-    private Player p;
+    private final Player p;
     private String name;
     private String time;
     private ItemStack item;
     private ItemMeta meta;
-    private ArrayList<String> lores;
     private String line1;
     private String line2;
     private String line3;
     private String line4;
 
+    private String line;
+
     private boolean isEnchant;
-    private boolean isName;
-    private boolean isTime;
+
 
     public Sign(Player p, ItemStack pi, String lore){
         this.p = p;
@@ -45,13 +38,11 @@ public class Sign {
         this.time = "normal";
         this.item = new ItemStack(pi);
         this.meta = item.getItemMeta();
-        this.lores = new ArrayList<>();
-        SignManager.signMap.put(p.getUniqueId(), this);
-        initLores();
-
-            lore = lore.replace("&", "§");
-            lore = "§7" + lore;
-            line1 = lore;
+        JenoSign.signMap.put(p.getUniqueId(), this);
+        line = "§7§m---------------------------------------";
+        lore = lore.replace("&", "§");
+        lore = "§7" + lore;
+        line1 = lore;
 
     }
 
@@ -60,11 +51,7 @@ public class Sign {
         item.setItemMeta(meta);
     }
 
-    private void initLores(){
-     for (int i = 0; i<5; i++){
-         lores.add(i,"");
-     }
-    }
+
     private String formatTime(final long millis) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
         return simpleDateFormat.format(millis);
@@ -99,20 +86,13 @@ public class Sign {
         }
         return month;
     }
+
     private String formatTimeYear(final long millis) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy");
         return simpleDateFormat.format(millis);
     }
 
-
-    public ItemStack createDeploySign(){
-
-        String z = manageTime(time);
-        String Signiert = manageName(name, p.getUniqueId());
-
-        ItemStack i = new ItemStack(item);
-        ItemMeta m = i.getItemMeta();
-        ArrayList<String> l= new ArrayList<>();
+    private void checkLinesNull(){
         if (line1 == null){
             line1 = "";
         }
@@ -125,6 +105,19 @@ public class Sign {
         if (line4 == null){
             line4 = "";
         }
+    }
+
+    public ItemStack createDeploySign(){
+
+        String z = manageTime(time);
+        String Signiert = manageName(name, p.getUniqueId());
+
+        ItemStack i = new ItemStack(item);
+        ItemMeta m = i.getItemMeta();
+        ArrayList<String> l= new ArrayList<>();
+
+        checkLinesNull();
+
         if (!line1.equalsIgnoreCase("")){
             l.add(line1);
         }
@@ -137,7 +130,7 @@ public class Sign {
         if (!line4.equalsIgnoreCase("")){
             l.add(line4);
         }
-        l.add("§7§m---------------------------------------");
+        l.add(line);
         l.add(Signiert + z);
         m.setLore(l);
 
@@ -198,6 +191,7 @@ public class Sign {
     public void setSignatureSettings(String[] s){
         this.time = s[0];
         this.name = s[1];
+        this.line = s[2];
     }
 
     public ItemStack createDeploySignSettings(String[] s){
@@ -231,7 +225,7 @@ public class Sign {
         if (!line4.equalsIgnoreCase("")){
             l.add(line4);
         }
-        l.add("§7§m---------------------------------------");
+        l.add(s[2]);
         l.add(Signiert + z);
         m.setLore(l);
 
@@ -261,8 +255,11 @@ public class Sign {
         return z;
     }
 
+
+
     private String manageName(String s, UUID uuid){
             String suffix = "";
+
         try {
             LuckPerms lp = LuckPermsProvider.get();
             User u = lp.getUserManager().loadUser(uuid).get();
@@ -281,133 +278,28 @@ public class Sign {
         }else return signiert + suffix + p.getName()+" ";
     }
 
-    public ItemStack createDeploySignLores(){
-
-        String Name = p.getName();
-        String Signiert = "§7Signiert von ";
-        ItemStack i = new ItemStack(item);
-        ItemMeta m = i.getItemMeta();
-        ArrayList<String> l= new ArrayList<>();
-        if (!line1.equalsIgnoreCase("")){
-            l.add(line1);
-        }
-        if (!line2.equalsIgnoreCase("")){
-            l.add(line2);
-        }
-        if (!line2.equalsIgnoreCase("")){
-            l.add(line2);
-        }
-        if (!line3.equalsIgnoreCase("")){
-            l.add(line3);
-        }
-        if (!line4.equalsIgnoreCase("")){
-            l.add(line4);
-        }
-        l.add("§7§m---------------------------------------");
-        l.add(Signiert + Name + " §7am §c" + formatTime(System.currentTimeMillis()));
-        m.setLore(l);
-
-        i.setItemMeta(m);
-        return i;
-    }
-/*
-    public void setSign(){
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String arg : args) {
-            stringBuilder.append(arg).append(" ");
-
-        }
-
-
-        if (meta.getLore() == null) {
-            lore = Lists.newArrayList();
-        } else {
-            lore = itemMeta.getLore();
-
-        }
-
-
-
-        if (plugin.getManagers().get(player).ColorItem) {
-
-            if (itemMeta.hasDisplayName()) {
-                String name = itemMeta.getDisplayName();
-
-
-                itemMeta.setDisplayName(plugin.getManagers().get(player).getColorSet().getColor(player.getUniqueId()) + plugin.getManagers().get(player).getColorSet().getFormat(player.getUniqueId()) + name);
-
-            } else {
-                String name;
-                name = itemStack.getType().name().toLowerCase().replace("_", " ");
-                String nameUp = Character.toUpperCase(name.charAt(0)) + name.substring(1);
-
-
-                itemMeta.setDisplayName(plugin.getManagers().get(player).getColorSet().getColor(player.getUniqueId()) + plugin.getManagers().get(player).getColorSet().getFormat(player.getUniqueId()) + nameUp);
-
-            }
-        }
-
-        String Name = player.getName();
-        String Signiert = "§7Signiert von ";
-
-        if(sm.isName()){
-            Name = "";
-            Signiert = "§7Signiert";
-        }
-
-        if(sm.isLine()){
-            lore.add("");
-            lore.add("");
-        }
-
-        if (sm.isTime()) {
-            lore.add("§7" + stringBuilder.toString().replace('&', '§'));
-            lore.add("§7§m---------------------------------------");
-            lore.add(Signiert + sm.getSignColor().setSignColor(player) + Name);
-            itemMeta.setLore(lore);
-
-        } else {
-            lore.add("§7" + stringBuilder.toString().replace('&', '§'));
-            lore.add("§7§m---------------------------------------");
-            lore.add(Signiert+ sm.getSignColor().setSignColor(player)+  Name + " §7am §c" + formatTime(System.currentTimeMillis()));
-            itemMeta.setLore(lore);
-        }
-
-        sm.getSignColor().removeColor(player);
-
-        if (sm.isEnchant()) {
-
-            itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-            itemMeta.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
-
-        }
-
-        itemStack.setItemMeta(itemMeta);
-        player.getInventory().setItemInMainHand(itemStack);
-        plugin.getManagers().remove(player);
+    private ChatColor manageHex(String suffix){
+        return ChatColor.valueOf(suffix);
     }
 
- */
 
     public void setEnchant(boolean enchant) {
         isEnchant = enchant;
     }
 
-    public Player getP() {
-        return p;
-    }
-
-    public void setP(Player p) {
-        this.p = p;
-    }
 
     public ItemStack getItem() {
         return item;
     }
     public ItemStack getItemNoDefaultName() {
         if (meta.hasDisplayName()) {
-            return item;
+            ItemStack i = new ItemStack(item);
+            ItemMeta m = i.getItemMeta();
+            String name = m.getDisplayName();
+            name = name.replace("§", "&");
+            m.setDisplayName(name);
+            i.setItemMeta(m);
+            return i;
         }else {
             ItemMeta m = item.getItemMeta();
             ItemStack i = new ItemStack(item);
@@ -459,42 +351,30 @@ public class Sign {
         return l;
     }
 
+    public void setLine(String line) {
+        this.line = line;
+    }
     public void setItem(ItemStack item) {
         this.item = item;
     }
-
     public ItemMeta getMeta() {
         return meta;
     }
-
     public void setMeta(ItemMeta meta) {
         this.meta = meta;
     }
-
-    public ArrayList<String> getLores() {
-        return lores;
-    }
-
-    public void setLores(ArrayList<String> lores) {
-        this.lores = lores;
-    }
-
     public boolean isEnchant() {
         return isEnchant;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String n) {
         name = n;
     }
-
     public String isTime() {
         return time;
     }
-
     public void setTime(String t) {
         time = t;
     }
